698b6f4aa5cd55d662d74ac4f54826ba49bc0fd8
Matthias Jung <m.jung@uni-wuerzburg.de>
2026-07-17 13:13:13 +0200

    fix: cron syntax

--- groupplanning/settings.py
+++ groupplanning/settings.py
@@ -178,6 +178,6 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@example.com')
 # Cron Jobs Configuration
 # https://github.com/hartwork/django-crontab
 CRONJOBS = [
-    ('0 8 * * *', 'django.core.management.call_command', ['send_notifications'], '>> /tmp/cron_notifications.log 2>&1'),
-    ('* * * * *', 'django.core.management.call_command', ['send_test_email'], '>> /tmp/cron_test_email.log 2>&1'),
+    ('0 8 * * *', 'django.core.management.call_command', ['send_notifications'], {}, '>> /tmp/cron_notifications.log 2>&1'),
+    ('* * * * *', 'django.core.management.call_command', ['send_test_email'], {}, '>> /tmp/cron_test_email.log 2>&1'),
 ]
